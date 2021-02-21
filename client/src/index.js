a824a5c1e2841734b8071cc38f13c03702a78d6b
wassimcss <wassimahmedmp21@gmail.com>
2021-02-21 20:23:31 +0100

    hk

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './store';
 import {Provider} from "react-redux"
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 
 
 ReactDOM.render(
